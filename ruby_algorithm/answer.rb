60f569a892213b89aa38679b0b61c155e9a81aff
masaengineer <masahiro.track.a.f@gmail.com>
2024-10-04 12:33:31 +0900

    課題1の追加

--- ruby_algorithm/answer.rb
+++ ruby_algorithm/answer.rb
@@ -1,5 +1,15 @@
 def algorithm_01(number)
   response = []
+  if number %3 == 0
+    response.push("Fizz")
+  elsif number %5 == 0
+    response.push("Buzz")
+  elsif number %3 == 0 && number %5 == 0
+    response.push("FizzBuzz")
+  else
+    response.push(number)
+  end
+  puts response
 end
 
 def algorithm_02(word)
